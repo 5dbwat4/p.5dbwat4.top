bbd26a7428f318ab50b0c9343612d6a910e7c604
5dbwat4 <7372467+z-jb@user.noreply.gitee.com>
2023-06-13 00:08:02 +0800

    u

--- thost/assets/5dbwat4-proj.thost.chunk.377.6e08c47b4d8545ff27fd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[377],{7377:(e,l,n)=>{n.r(l),n.d(l,{default:()=>a});var s=n(7875),u=n(349),t=n(641);const a={setup(e){const l=(0,t.iH)("Loading");return fetch("/api/list/v2/all_uuids").then((e=>e.json())).then((e=>{console.log(e),l.value=e.map((e=>e.id)).join(",")})),(e,n)=>((0,s.wg)(),(0,s.iD)("p",null,(0,u.zw)(l.value),1))}}}}]);
